4d34b2b533cab0e0f08baab3ccc62c1d87a6c8e1
Todd Moubray <>
2023-04-14 16:00:04 -0400

    Add procfile.  Fix cors.

--- app.js
+++ app.js
@@ -14,6 +14,21 @@ const dbConnect = require("./db/dbConnect");
 dbConnect();
 
 
+// Curb Cores Error by adding a header here
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+
 // body parser configuration
 app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
